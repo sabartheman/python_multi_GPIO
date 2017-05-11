72bb725dd6802d66f2333b0e5e1c6eb2782c621e
sabartheman <skylar4494@gmail.com>
2017-05-11 15:39:52 -0600

    made some changes

--- justToggling/pin1test.py
+++ justToggling/pin1test.py
@@ -1,16 +1,17 @@
 import RPi.GPIO as GPIO
-import time.sleep as sleep
+from time import sleep
 
 
 #pin 23 has a pull down resistor
 #pin 24 has a pull up   resistor
-GPIO.setmode(BCM)
+GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+GPIO.setup(27,OUTPUT)
 
 def toggle_a_single_pin_1hz():
-    GPIO.output(27, HIGH)
+    GPIO.output(27, GPIO.HIGH)
     sleep(.5)
-    GPIO.output(27, LOW)
+    GPIO.output(27, GPIO.LOW)
     sleep(.5)
 
 def main():
--- justToggling/pin2test.py
+++ justToggling/pin2test.py
@@ -1,21 +1,22 @@
 import RPi.GPIO as GPIO
-import time.sleep as sleep
+from time import sleep
 
 
 #pin 23 has a pull down resistor
 #pin 24 has a pull up   resistor
-GPIO.setmode(BCM)
-GPIO.setwarning(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(10,OUTPUT)
 
-def toggle_a_single_pin_2hz():
-    GPIO.output(10, HIGH)
-    sleep(1)
-    GPIO.output(10, LOW)
-    sleep(1)
+def toggle_a_single_pin_1hz():
+    GPIO.output(10, GPIO.HIGH)
+    sleep(.5)
+    GPIO.output(10, GPIO.LOW)
+    sleep(.5)
 
 def main():
     while(True):
-        toggle_a_single_pin_2hz()
+        toggle_a_single_pin_1hz()
 
 if __name__ == '__main__':
     main()
